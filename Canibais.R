6d0f0a4095f079ad3bc5643c44351137821650ce
samara <samara@Samaras-MacBook-Pro.local>
2018-10-06 11:38:13 -0300

    bla

--- Canibais.R
+++ Canibais.R
@@ -1,7 +1,7 @@
 source("Estado.R")
 
 ## Classe e métodos para o problema dos 3 Missionários e 3 Canibais
-Canibais <- function(desc = NULL, pai = NULL){
+QuebraCabeca <- function(desc = NULL, pai = NULL){
 
   e <- environment()
   
@@ -11,20 +11,20 @@ Canibais <- function(desc = NULL, pai = NULL){
   assign("h", Inf, envir = e)
   assign("f", Inf, envir = e)
   
-  class(e) <- c("Canibais", "Estado")
+  class(e) <- c("QuebraCabeca", "Estado")
 
   return(e)
 }
 
 ## Sobrecarregando o operador "==" para comparação entre estados
-Ops.Canibais = function(obj1,obj2){
+Ops.QuebraCabeca = function(obj1,obj2){
   if(.Generic == "=="){
     return(all(obj1$desc == obj2$desc))
   }
 }
 
 ## Sobrecarga da função genérica "print" do R
-print.Canibais <- function(obj) {
+print.QuebraCabeca <- function(obj) {
   cat("(M C B): (", obj$desc, ")\n")
   cat("G(n): ", obj$g, "\n")
   cat("H(n): ", obj$h, "\n")
@@ -32,15 +32,19 @@ print.Canibais <- function(obj) {
 }
 
 ## Sobrecarga da função genérica "heuristica", definida por Estado.R
-heuristica.Canibais <- function(atual){
+heuristica.QuebraCabeca <- function(atual){
+  
+  ## Se uma peca nao estiver na sua posicao objetivo,
+  ## somar a distancia entre sua posicao nos eixos x
+  ## e y e incrementar a heuristica
   
   if(is.null(atual$desc))
     return(Inf)
-  ## h(obj) = M + C + B
-  return(sum(atual$desc))
+  h(obj) = P11 + P12 + P21 + P22 + P31 + P32 + P41 + P42 + P51 + P52 + P61 + P62 + P71 + P72 + P81 + P82
+  return(sum(h(obj)))
 }
 
-geraFilhos.Canibais <- function(obj) {
+geraFilhos.QuebraCabeca <- function(obj) {
   
   filhos <- list()
   
@@ -48,7 +52,7 @@ geraFilhos.Canibais <- function(obj) {
 
   desc <- obj$desc
   
-  bAtual <- as.numeric(desc[3])
+  bAtual <- as.numeric(desc[16])
   
   bNovo <- as.numeric(bAtual != 1)
   
@@ -61,7 +65,7 @@ geraFilhos.Canibais <- function(obj) {
     
   } else {
     
-    operadores <- list(c(2,0,bNovo), c(0,2,bNovo), c(1,1,bNovo), c(1,0,bNovo), c(0,1,bNovo))
+    operadores <- list(c(1,0,bNovo), c(-1,0,bNovo), c(0,1,bNovo), c(0,-1,bNovo))
     
     filhosDesc <- lapply(operadores, function(op) desc+op)
   }
@@ -70,9 +74,8 @@ geraFilhos.Canibais <- function(obj) {
   incompativeis <- sapply(1:length(filhosDesc),
                     function(i) {
                       fDesc <- filhosDesc[[i]]
-                      if((fDesc['C'] > fDesc['M']) || ## Se #Canibais > #Missionários OU
-                         (any(fDesc[1:2] > 3)) ||     ##    #Canibais ou #Missionários > 3 OU
-                         (any(fDesc[1:2] < 0)))       ##    #Canibais ou #Missionarios < 0 então
+                      if((any(fDesc[1:2] > 2)) || ## Se algum eixo ultrapassar a posicao 2
+                         (any(fDesc[1:2] < 0)))   ## Se algum eixo ultrapassar a posicao 0
                         i ## é incompatível: retorna índice
                       else
                         0 ## senão é compatível
@@ -84,9 +87,9 @@ geraFilhos.Canibais <- function(obj) {
   ## remove estados filhos incompatíveis
   filhosDesc <- filhosDesc[-incompativeis]
   
-  ## gera os objetos Canibais para os filhos
+  ## gera os objetos QuebraCabeca para os filhos
   for(filhoDesc in filhosDesc){
-    filho <- Canibais(desc = filhoDesc, pai = obj)
+    filho <- QuebraCabeca(desc = filhoDesc, pai = obj)
     filho$h <- heuristica(filho)
     filho$g <- obj$g + 1
     filhos <- c(filhos, list(filho))
